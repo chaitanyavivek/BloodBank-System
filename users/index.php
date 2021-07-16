fd45999b69cdaa63375f8b537ee4be2d8b8cc776
Chaitanya vivek <chaitanyavivek7@gmail.com>
2021-07-16 22:40:16 +0530

    blood bank using web

---
+++ users/index.php
@@ -0,0 +1,37 @@
+<?php
+$title = "Home";
+$setHomeActive = "active";
+include 'layout/_header.php';
+
+include 'layout/navbar.php';
+?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-10">
+            <a href="#" id="Heading">Why Are We Here For?</a>
+            <p id="About">
+                Blood Bank System (BBS) is a browser based system that is designed to store, process, retrieve and analyze information concerned with the administrative and inventory management within a blood bank. This project aims at maintaining all the information pertaining to blood donors, different blood groups available in each blood bank and help them manage in a better way. 
+            </p>
+            <hr id="line_1">
+            <a href="#" id="Heading2">Features--></a>
+            <p>
+
+                - Blood Donation Camp & Camp Organiser Management.<br>
+- Donor Management - Donor Registration, Managing donor database, recording their physical and medical statistics.<br>
+- Inventory management in blood bank for storage and issuance of blood.<br>
+- Blood requisition and issuance of blood.<br>
+- Online transfer of blood from one blood bank to another.<br>
+- List of Donors who are eligible for donation on a particular date with contact Number.<br>
+- Camp Wise Donor List and Printing of Donor Cards.<br>
+            </p>
+        </div>
+        <div class="col-md-2">
+            <img src="../assets/donate_blood_right.jpg" class="img img-responsive img-thumbnail" style="width:200px ;">
+             <img src="http://bbmspunjab.gov.in/images/bbmsRht.png" class="img img-responsive img-thumbnail">
+        </div>
+
+
+
+    </div>
+</div>
+
